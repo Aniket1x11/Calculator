62bb78b9fc404f7897ee90d59bb47f1ec5da5d80
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-06-30 22:31:53 +0530

    add divide functionality

--- src/simple/Calculator.java
+++ src/simple/Calculator.java
@@ -10,6 +10,7 @@ public class Calculator
         System.out.println(calculator.add(5,6));
         System.out.println(calculator.subtract(56,23));
         System.out.println(calculator.multiply(5,23));
+        System.out.println(calculator.divide(81,27));
     }
     
     public int add(int num1, int num2)
@@ -25,5 +26,10 @@ public class Calculator
     public int multiply(int num1, int num2)
     {
         return num1 * num2;
+    }
+    
+    public int divide(int num1, int num2)
+    {
+        return num1 / num2;
     }
 }
